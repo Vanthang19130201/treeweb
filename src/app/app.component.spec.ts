364e6a05cab8bf3b7802b5bb47582f07e1c3c497
0333537185 <104660582+0333537185@users.noreply.github.com>
2023-11-06 10:20:51 +0700

    vanthang

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,23 +2,27 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  // @ts-ignore
   beforeEach(() => TestBed.configureTestingModule({
     declarations: [AppComponent]
   }));
 
   it('should create the app', () => {
+    // @ts-ignore
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'treeweb'`, () => {
+    // @ts-ignore
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('treeweb');
   });
 
   it('should render title', () => {
+    // @ts-ignore
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HompePageComponent } from './components/hompe-page/hompe-page.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HompePageComponent,
+    ProductsComponent,
+    BlogComponent,
+    ContactComponent,
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
