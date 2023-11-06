4d3ab0255538173ce5f9f3e006abf2254fa3dfcf
0333537185 <104660582+0333537185@users.noreply.github.com>
2023-11-06 23:41:13 +0700

    vanthang

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,16 @@ import {ProductsComponent} from "./components/products/products.component";
 import {RouterModule, Routes} from "@angular/router";
 import {ContactComponent} from "./components/contact/contact.component";
 import {BlogComponent} from "./components/blog/blog.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+// @ts-ignore
 const authRoutes: Routes = [
   { path: 'home-page', component: HompePageComponent},
   { path: 'products', component: ProductsComponent},
   { path: 'contact', component: ContactComponent},
   { path: 'blog', component: BlogComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
   {
     path:'', redirectTo:'home-page', pathMatch:'full'
   }
